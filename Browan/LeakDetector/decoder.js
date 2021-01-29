2320435261a37525c25b90faac4cd8c40ca07ff1
jas-williams <73598288+jas-williams@users.noreply.github.com>
2021-01-29 23:14:00 +0000

    First attempt at the Leak Detector code

---
+++ Browan/LeakDetector/decoder.js
@@ -0,0 +1,48 @@
+/* 
+ * Decoder function for Helium
+ *
+ * This function was created by Jason
+ */
+
+function Decoder(bytes, port) {
+
+    var params = {
+        "bytes": bytes
+    };
+
+    // Status measurement
+    params.leak = ((bytes[0] & 0x1) !== 0) ? true : false;
+    params.waterleakint = ((bytes[8] & 0x4) !== 0) ? true : false;
+    params.temp_change = ((bytes[8] & 0x5) !== 0) ? true : false;
+    params.RH_change = ((bytes[8] & 0x6) !== 0) ? true : false;
+    
+    
+    
+
+    // Humidity Measurement
+    rh = bytes[3] &= 0x7f;
+    if (rh === 127) {
+        rh_error = true;
+    } else {
+        rh_error = false;
+    }
+    
+    // Board temp measurement
+    temp_board = bytes[2] & 0x7f;
+    temp_board = temp_board - 32;
+
+    // Ambient temp measurement
+    temp_ambient = bytes[4] & 0x7f;
+    temp_ambient = temp_ambient - 32;
+
+    // Battery measurements
+    batt = bytes[1] & 0x0f;
+    batt = (25 + batt) / 10;
+    params.rh = rh;
+    params.rh_error = rh_error;
+    params.temp_board = temp_board;
+    params.temp_ambient = temp_ambient;
+    params.batt = batt;
+
+    return params;
+}
