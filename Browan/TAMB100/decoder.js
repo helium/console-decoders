9b7820fbd891e5be9bf9216570df36c436c141ad
jas-williams <73598288+jas-williams@users.noreply.github.com>
2021-02-01 12:47:33 +0000

    Create decoder.js

---
+++ Browan/TAMB100/decoder.js
@@ -0,0 +1,36 @@
+/* 
+ * Decoder function for Helium
+ * Browan Ambient Light Sensor
+ *
+ * This function was created by Jason
+
+function Decoder(bytes, port) {
+
+    var params = {
+        "bytes": bytes
+    };
+
+    // Status measurement
+	params.darker = ((bytes[0] & 0x0) !== 0) ? true : false;
+	params.lighter = ((bytes[0] & 0x1) !== 0) ? true : false;
+	params.keep_alive = ((bytes[0] & 0x4) !== 0) ? true : false;
+	   
+	// Lux
+	params.lux = ((bytes[5] << 16) | (bytes[4] << 8) | bytes[3])/100;
+
+
+    
+    // Board temp measurement
+    temp_board = bytes[2] & 0x7f;
+    temp_board = temp_board - 32;
+
+
+
+    // Battery measurements
+    batt = bytes[1] & 0x0f;
+    batt = (25 + batt) / 10;
+    params.temp_board = temp_board;
+    params.batt = batt;
+
+    return params;
+}
