2a045c6419dc4a327fa5017cd2fa39a18c658d92
jas-williams <73598288+jas-williams@users.noreply.github.com>
2021-02-05 12:14:16 +0000

    Add Battery Level

--- Dragino/LHT65/decoder.js
+++ Dragino/LHT65/decoder.js
@@ -1,44 +1,77 @@
 function Decoder(bytes, port) {
-  //Payload Formats of LHT65 Deveice
-  var exti = bytes[8] & 0x01 ? "TRUE" : "FALSE"; //
-
+//Payload Formats of LHT65 Deveice
   return {
-    //External sensor
-    Ext_sensor: {
-      "0": "No external sensor",
-      "1": "Temperature Sensor",
-      "2": "Door Sensor",
-      "3": "Water Leak Sensor",
-    }[bytes[6] & 0xff],
+    
+       //Battery Level
+       Bat_Level:
+       {
+         "0":"Ultra Low",
+         "1":"Low",
+         "2":"OK",
+         "3":"Good",
+       }
+       [((bytes[0]<<8 | bytes[1])>>14)&0xFF],
+       
+       //External sensor
+       Ext_sensor:
+       {
+         "0":"No external sensor",
+         "1":"Temperature Sensor",
+         "4":"Interrupt/Door Sensor send",
+         "5":"Illumination Sensor",
+         "6":"ADC Sensor",
+         "7":"Interrupt Sensor count",
+       }[bytes[6]&0x7F],
+       
+       //Battery,units:V
+       BatV:((bytes[0]<<8 | bytes[1]) & 0x3FFF)/1000,
+       
+       //SHT20,temperature,units:
+       TempC_SHT:((bytes[2]<<24>>16 | bytes[3])/100).toFixed(2),
+       
+       //SHT20,Humidity,units:%
+       Hum_SHT:((bytes[4]<<8 | bytes[5])/10).toFixed(1),
+       
+       //DS18B20,temperature,units:
+       TempC_DS:
+       {
+         "1":((bytes[7]<<24>>16 | bytes[8])/100).toFixed(2),
+       }[bytes[6]&0xFF],       
+       
+       //Exit pin level,PA4
+       Exit_pin_level:
+       {
+         "4":bytes[7] ? "High":"Low",
+       }[bytes[6]&0x7F], 
+       
+       //Exit pin status,PA4
+       Exit_status:
+       {
+         "4":bytes[8] ? "True":"False",
+       }[bytes[6]&0x7F],    
+       
+       //BH1750,illumination,units:lux
+       ILL_lx:
+       {
+         "5":bytes[7]<<8 | bytes[8],
+       }[bytes[6]&0x7F],  
 
-    //Battery,units:V
-    BatV: (((bytes[0] << 8) | bytes[1]) & 0x3fff) / 1000,
-
-    //SHT20,temperature,units:â„ƒ
-    TempC_SHT: ((((bytes[2] << 24) >> 16) | bytes[3]) / 100).toFixed(2),
-
-    //SHT20,Humidity,units:%
-    Hum_SHT: (((bytes[4] << 8) | bytes[5]) / 10).toFixed(1),
-
-    //DS18B20,temperature,units:â„ƒ
-    TempC_DS: {
-      "1": ((((bytes[7] << 24) >> 16) | bytes[8]) / 100).toFixed(2),
-    }[bytes[6] & 0xff],
-
-    //status of door sensor
-    Door_status: {
-      "2": bytes[7] & 0x01 ? "OPEN" : "CLOSE",
-    }[bytes[6] & 0xff],
-
-    //status of water sensor
-    Water_status: {
-      "3": bytes[7] & 0x01 ? "NORMAL" : "LEAK",
-    }[bytes[6] & 0xff],
-
-    //status of exti_trigger
-    EXTI_Trigger: {
-      "2": exti,
-      "3": exti,
-    }[bytes[6] & 0xff],
+        //ADC,PA4,units:V
+        ADC_V:
+       {
+         "6":(bytes[7]<<8 | bytes[8])/1000,
+       }[bytes[6]&0x7F],  
+       
+        //Exit count,PA4,units:times
+        Exit_count:
+        {
+          "7":bytes[7]<<8 | bytes[8],
+        }[bytes[6]&0x7F],  
+        
+        //Applicable to working mode 4567,and working mode 467 requires short circuit PA9 and PA10
+        No_connect:
+        {
+          "1":"Sensor no connection",
+        }[(bytes[6]&0x80)>>7],  
   };
 }
