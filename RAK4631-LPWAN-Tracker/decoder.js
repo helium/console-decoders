8c04d025f9d38bec2be080087908be4a7e668915
arkieguy <wbrown@arkie.net>
2021-04-22 10:44:03 -0500

    Update decoder.js
    Used names compatible with Cargo.

--- RAK4631-LPWAN-Tracker/decoder.js
+++ RAK4631-LPWAN-Tracker/decoder.js
@@ -4,11 +4,11 @@ function Decoder(bytes, port) {
   
   var sensor = {};
   
-  sensor.lat = (bytes[0] | bytes[1] << 8 | bytes[2] << 16 | bytes[3] << 24 | (bytes[3] & 0x80 ? 0xFF << 24 : 0)) / 100000.0;
-  sensor.lon = (bytes[4] | bytes[5] << 8 | bytes[6] << 16 | bytes[7] << 24 | (bytes[7] & 0x80 ? 0xFF << 24 : 0)) / 100000.0;
-  sensor.alt = (bytes[8] | bytes[9] << 8 | (bytes[9] & 0x80 ? 0xFF << 16 : 0));
-  sensor.pr  = bytes[10];
-  sensor.bat = bytes[11];
+  sensor.latitude = (bytes[0] | bytes[1] << 8 | bytes[2] << 16 | bytes[3] << 24 | (bytes[3] & 0x80 ? 0xFF << 24 : 0)) / 100000.0;
+  sensor.longitude = (bytes[4] | bytes[5] << 8 | bytes[6] << 16 | bytes[7] << 24 | (bytes[7] & 0x80 ? 0xFF << 24 : 0)) / 100000.0;
+  sensor.altitude = (bytes[8] | bytes[9] << 8 | (bytes[9] & 0x80 ? 0xFF << 16 : 0));
+  // sensor.pr  = bytes[10];
+  sensor.battery = bytes[11];
   
   return( sensor );
 }
