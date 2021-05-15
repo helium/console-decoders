63465d7f5c7107998129e29d7d0ff1f725ac23c6
Florian Mauduit <f@lf.je>
2021-05-15 02:18:31 -0400

    Add Wisblock1906 Sensor Decoder

---
+++ Wisblock/1906/decoder.js
@@ -0,0 +1,10 @@
+function Decoder(bytes, port) {
+    var decoded = {};
+    
+    decoded.temperature = (bytes[1] << 8 | (bytes[2])) / 100;
+    decoded.humidity = (bytes[3] << 8 | (bytes[4])) / 100;
+    decoded.pressure = (bytes[8] | (bytes[7] << 8) | (bytes[6] << 16) | (bytes[5] << 24)) / 100;
+    decoded.gas = bytes[12] | (bytes[11] << 8) | (bytes[10] << 16) | (bytes[9] << 24);
+  
+    return decoded;
+  }
